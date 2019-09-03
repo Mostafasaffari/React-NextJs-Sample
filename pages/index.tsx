97538733de2ae4dfe2d57665aff981180da0fb56
mostafa <mostafasaffari@gmail.com>
2019-09-04 00:10:02 +0430

    fetch cities

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
+import { NextComponentType } from "next";
+import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 
-const Index = () => {
+interface ICity {
+  name: string;
+  slug: string;
+  latitude: string;
+  longitude: string;
+}
+interface IProps {
+  cities: Array<ICity>;
+}
+const Index: NextComponentType<{}, {}, IProps> = ({ cities }) => {
   return (
     <h1>
       Hello Mostafa{" "}
@@ -9,6 +20,12 @@ const Index = () => {
       </Link>
     </h1>
   );
+};
+
+Index.getInitialProps = async () => {
+  const response = await fetch("https://plan.1stquest.com/api/v1/cities");
+  const data = await response.json();
+  return { cities: data.data };
 };
 
 export default Index;
