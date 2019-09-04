7339e99dad862c5659932956a29d3e911f4bb609
mostafa <mostafasaffari@gmail.com>
2019-09-04 13:08:30 +0430

    design home complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import fetch from "isomorphic-unfetch";
 import Layout from "../components/layout";
 import Header from "../components/header";
 
-
 import "../assets/css/home/home.css";
 
 interface ICity {
@@ -26,6 +25,18 @@ const Index: NextComponentType<{}, {}, IProps> = ({ cities }) => {
   return (
     <Layout>
       <Header />
+      <div id="slogan" className="container mx-auto max-w-5xl text-white pt-20">
+        <h2 className="text-5xl font-bold tracking-wide">
+          Plan Your trip to Iran
+        </h2>
+        <p className="max-w-2xl text-2xl tracking-wide">
+          Find or create your best plan to visit Iran based on +300 plans
+          designed by professional tour guides and travelers
+        </p>
+      </div>
+      <div id="filterDivider" className="h-20 bg-blue-100 mt-10">
+        
+      </div>
     </Layout>
   );
 };
