81fa51124f107c8d3e9b044981b624b660d3a129
mostafa <mostafasaffari@gmail.com>
2019-09-06 08:19:22 +0430

    add push to home if not set filter in result page

--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -1,13 +1,36 @@
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 import { IFilter } from "../../interfaces/IFilter";
 
 import Layout from "../../components/layout";
+import { useEffect } from "react";
 
 const Results = () => {
   const filters: IFilter = useSelector<IFilter>(state => state.Filters);
   const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      !filters.cities.length ||
+      !filters.durations.length ||
+      !filters.dates.length ||
+      !filters.passengers.length
+    ) {
+      router.push("/");
+    }
+  }, []);
+
+  if (
+    !filters.cities.length ||
+    !filters.durations.length ||
+    !filters.dates.length ||
+    !filters.passengers.length
+  ) {
+    return null;
+  }
   return (
     <Layout>
       <h1>{filters.cities}</h1>
