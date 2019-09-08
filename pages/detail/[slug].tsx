03569970b9262ddfbf3ee23c0d4d336004079e69
mostafa <mostafasaffari@gmail.com>
2019-09-08 21:23:57 +0430

    start creating detail page

--- pages/detail/[slug].tsx
+++ pages/detail/[slug].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 const Detail = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <h1>Detail {slug}</h1>;
+  return <h1>this is detail page {slug}</h1>;
 };
 
 export default Detail;
