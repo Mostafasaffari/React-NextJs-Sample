1d7ef0e3e33657428b62cdecf4d18c4a1633b4d3
mostafa <mostafasaffari@gmail.com>
2019-09-14 09:16:32 +0430

    simple detail design

--- pages/detail/[slug].tsx
+++ pages/detail/[slug].tsx
@@ -1,14 +1,16 @@
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { BaseContext, NextComponentType } from "next-server/dist/lib/utils";
 
+import { IError } from "../../interfaces/IError";
 import { IItinerary } from "../../interfaces/IItinerary";
 
 import { getItinerariesBySlug } from "../../services/apiItinerary";
 
 import Layout from "../../components/layout/layout";
 import Header from "../../components/header/header";
-import { useEffect } from "react";
-import { IError } from "../../interfaces/IError";
+
+import "../../assets/css/detail.css";
 
 interface IProps {
   itinerary: IItinerary | null;
@@ -28,24 +30,24 @@ const Detail: NextComponentType<{}, {}, IProps> = ({
       alert(error.message);
     }
     if (itinerary === null) {
-      router.push("/");
+      //  router.push("/");
     }
   }, []);
   if (itinerary === null) {
-    return null;
+    // return null;
   }
   return (
     <Layout className="bgdefault bg-no-repeat xl:bg-cover">
       <Header />
       <div
         id="details"
-        className="container mx-auto max-w-5xl text-white pt-20 md:pl-10 xl:pl-0 md:mb-10"
+        className="container mx-auto max-w-6xl text-white pt-20 md:pl-10 xl:pl-0 md:mb-10"
       >
         <span>1stQuest / Iran Trip Planner</span>
-        <h2 className="text-5xl font-bold tracking-wide">
+        <h2 className="text-5xl md:text-3xl font-bold tracking-wide">
           9 days in Tehran, Shiraz, and Isfahan
         </h2>
-        <p className="max-w-2xl text-2xl tracking-wide">
+        <p className="max-w-3xl text-1xl tracking-wide">
           During your Persia travel, you will explore Iran’s rich history and
           amazing culture visiting the most famous historical cities of Persia
           in 11 days of Persia Tour. This package fits those who seek to have a
@@ -54,6 +56,12 @@ const Detail: NextComponentType<{}, {}, IProps> = ({
           experience in what they do. They will help you with everything during
           your Persia tour.
         </p>
+        <a href="https://1stquest.com" className="underline">Read Story of this plan on <span className="text-yellow-500"> 1stQuest Blog</span></a>
+      </div>
+      <div className="timeline">
+        <div className="start-timeline">
+          <span>Trip Timeline</span>
+        </div>
       </div>
     </Layout>
   );
